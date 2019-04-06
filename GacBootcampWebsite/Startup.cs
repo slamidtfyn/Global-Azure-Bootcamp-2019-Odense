a268d14502667e19e58e9e2d5434b52cb20d27da
mikkelhm <mikkel@umbraco.com>
2019-04-06 08:49:30 +0200

    Controller pr. topic

--- GacBootcampWebsite/Startup.cs
+++ GacBootcampWebsite/Startup.cs
@@ -24,14 +24,6 @@ namespace GacBootcampWebsite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -48,10 +40,7 @@ namespace GacBootcampWebsite
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
-
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
