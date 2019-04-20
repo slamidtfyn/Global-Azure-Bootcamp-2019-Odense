55cdc48c7275021f71b1c2dcda5968c5deb76171
mclausen <martinh.c@me.com>
2019-04-20 19:34:22 +0200

    Added service bus section

--- GacBootcampWebsite/Controllers/ServiceBusController.cs
+++ GacBootcampWebsite/Controllers/ServiceBusController.cs
@@ -1,16 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using GacBootcampWebsite.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
 
 namespace GacBootcampWebsite.Controllers
 {
     public class ServiceBusController : Controller
     {
+        const int InternalServerErrorHttpStatusCode = 500;
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult SendEmail(SendEmailRequest sendEmailRequest)
+        {
+            var random = new Random();
+            Thread.Sleep(random.Next(3000, 8000));
+
+            if(random.Next(0, 1) > 0)
+                return StatusCode(InternalServerErrorHttpStatusCode);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ GacBootcampWebsite/Models/SendEmailRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GacBootcampWebsite.Models
+{
+    public class SendEmailRequest
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Subject")]
+        [DataType(DataType.Text)]
+        public string Subject { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Message")]
+        [DataType(DataType.Text)]
+        public string Message { get; set; }
+    }
+}
