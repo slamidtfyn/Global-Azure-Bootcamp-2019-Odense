e5776eaeadaa26ba7a62138e4c3c43b835e1470f
mikkelhm <mikkel@umbraco.com>
2019-04-25 23:12:11 +0200

    Add azure function http trigger example

--- GacBootcampWebsite/Controllers/FunctionsController.cs
+++ GacBootcampWebsite/Controllers/FunctionsController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using GacBootcampWebsite.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GacBootcampWebsite.Controllers
@@ -9,6 +11,32 @@ namespace GacBootcampWebsite.Controllers
     public class FunctionsController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SendEmail(SendEmailRequest sendEmailRequest)
+        {
+            var functionUrl = "https://globalazurebootcampodense.azurewebsites.net/api/HttpTrigger1?code=gVBiAVaXsCnhHqXR7I19jga679nVGoLke266nDvWdVV6OdfLuME4TA==";
+            using (var client = new HttpClient())
+            {
+                // OFFload to Azure Functions
+                var result = await client.PostAsJsonAsync<SendEmailRequest>(functionUrl, sendEmailRequest);
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Success");
+                }
+            }
+            // At this point it fialed to send to the endpoint, we shuold handle it
+            return RedirectToAction("Fail");
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        public IActionResult Fail()
         {
             return View();
         }
