a2af0391c26d779d35c50e2c15b588f741152e97
pijemcolu <mklstmnk@gmail.com>
2019-04-26 15:09:13 +0200

    Add db migration

---
+++ GacBootcampWebsite/Migrations/20190426125509_InitialMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GacBootcampWebsite.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    FirstName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.FirstName);
+                });
+
+            migrationBuilder.InsertData("Users", "FirstName", new string[] { "Robert is a god", "Mikulas", "Someone Else" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
